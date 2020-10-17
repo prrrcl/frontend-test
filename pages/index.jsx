45240c37e745e77618111ab39461c3b332266d82
Adri <adrianporcelnavarro@hotmail.com>
2020-10-17 17:13:10 +0200

    added styles on grid

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,22 +37,24 @@ const Home = () => {
               <img src="/profile.jpg" />
             </div>
             <div className="column">
-              <h2>Sobre mí</h2>
-              <p>
-                Mi visión: esa creación, cualquiera que sea la forma que adopte, debe mejorar la
-                vida de tantas personas. como sea posible. Creo con vehemencia que este deber
-                poético y político, rebelde y benevolente, pragmático y subversivo debe ser
-                compartido por todos.
-              </p>
-              <p>
-                A los ojos de este consumado ciudadano del mundo, compartir su visión ética y
-                humanista de un planeta más igualitario es un deber, si no un imperativo moral, que
-                se traduce en proyectos poco convencionales que traen fértiles sorpresas. Es fácil
-                adivinar su curso de acción: un objeto debe ser útil antes de ser hermoso.
-              </p>
-              <Link href="/portfolio">
-                <a className="button button--hollow">Ver mi portfolio</a>
-              </Link>
+              <div className="content-wrapper">
+                <h2>Sobre mí</h2>
+                <p>
+                  Mi visión: esa creación, cualquiera que sea la forma que adopte, debe mejorar la
+                  vida de tantas personas. como sea posible. Creo con vehemencia que este deber
+                  poético y político, rebelde y benevolente, pragmático y subversivo debe ser
+                  compartido por todos.
+                </p>
+                <p>
+                  A los ojos de este consumado ciudadano del mundo, compartir su visión ética y
+                  humanista de un planeta más igualitario es un deber, si no un imperativo moral, que
+                  se traduce en proyectos poco convencionales que traen fértiles sorpresas. Es fácil
+                  adivinar su curso de acción: un objeto debe ser útil antes de ser hermoso.
+                </p>
+                <Link href="/portfolio">
+                  <a className="button button--hollow">Ver mi portfolio</a>
+                </Link>
+              </div>
             </div>
           </div>
         </section>
